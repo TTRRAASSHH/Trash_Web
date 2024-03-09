9443a5b700150a2e2c20f45fcf2e9ad3b1611861
huise0ng <shinhuiseong07@gmail.com>
2024-03-10 01:00:52 +0900

    ADD :: firebase.js

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+/* import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDsdK7WxMhlfphf7ggrtX2XS2lJ3_T1rEU",
+  authDomain: "trash-99a7e.firebaseapp.com",
+  projectId: "trash-99a7e",
+  storageBucket: "trash-99a7e.appspot.com",
+  messagingSenderId: "376601873749",
+  appId: "1:376601873749:web:4997b4f6a96580d458b3dc"
+};
+
+const app = initializeApp(firebaseConfig);
+*/
